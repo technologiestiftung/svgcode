d2904ca9236d01a17aabf3013d07bdb40bcfc274
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-07-27 08:29:36 +0200

    remove dist folder

--- dist/common/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GCodeCommands = void 0;
-var GCodeCommands;
-(function (GCodeCommands) {
-    GCodeCommands["lift"] = "G0 Z10";
-    GCodeCommands["feedrate5000"] = "F5000";
-    GCodeCommands["goHome"] = "G0 X0 Y0";
-})(GCodeCommands = exports.GCodeCommands || (exports.GCodeCommands = {}));
